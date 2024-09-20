3df80b6dddb199f93e4c2ef42fc0e719b2251f91
ZhasBas <159027094+ZhasBas@users.noreply.github.com>
2024-09-20 16:26:13 +0500

    Add files via upload

--- facecaptcha-main/base/models.py
+++ facecaptcha-main/base/models.py
@@ -3,5 +3,12 @@ from datetime import datetime, timezone, timedelta
 # Create your models here.
 class UserInfo(models.Model):
     username = models.CharField(blank=False, max_length=150)
-    sex = models.CharField(blank=False, max_length=10)
+    sex = models.CharField(blank=True, max_length=10)
+    job = models.CharField(blank=True, max_length=50)
+    housing = models.CharField(blank=True, max_length=50)
+    savings = models.CharField(blank=True, max_length=50)
+    checking = models.CharField(blank=True, max_length=50)
+    amount = models.CharField(blank=True, max_length=50)
+    duration = models.CharField(blank=True, max_length=50)
+    purpose = models.CharField(blank=True, max_length=50)
     chats = models.JSONField(blank=True, null=True)
